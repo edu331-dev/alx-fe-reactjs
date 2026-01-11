2a79b8bb8d1090c2eff67c6e33d399cb1891f995
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-11 09:21:37 +0300

    update for the jsx files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,18 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 
 function App() {
     return (
         <div>
+            <WelcomeMessage />
+
+            <Header />
+            <MainContent />
+            <Footer />
+
             <UserProfile
                 name="Alice"
                 age="25"
