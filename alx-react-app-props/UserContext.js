bb948ddabe79fa36b26228635a89a2718312e7b0
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 16:51:10 +0300

    tasks 2 update

--- alx-react-app-props/UserContext.js
+++ alx-react-app-props/UserContext.js
@@ -1,6 +1,16 @@
-import { createContext } from 'react';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
-// Create the context
-const UserContext = createContext(null);
+function UserDetails() {
+  const userData = useContext(UserContext);
 
-export default UserContext;
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
+
