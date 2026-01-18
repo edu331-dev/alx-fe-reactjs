d89e3162138753a418d7c69d8e9f0bcb3890bc07
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 13:49:58 +0300

    Task 0

--- alx-react-app-new/src/main.jsx
+++ alx-react-app-new/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#f5f5f5",
+        minHeight: "200px"
+      }}
+    >
+      <p style={{ fontSize: "16px", lineHeight: "1.5" }}>
+        This application showcases inline CSS styling in React components.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
