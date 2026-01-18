d8860c21ba4d0a81cadbf1980a36d19b3d0daaca
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 15:07:04 +0300

    Task 1 update 2

---
+++ alx-react-app-new/src/components/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Counter from './components/Counter'; // Import Counter component
+
+function App() {
+  return (
+    <div>
+      <Counter />   {/* Use the Counter component here */}
+    </div>
+  );
+}
+
+export default App;
