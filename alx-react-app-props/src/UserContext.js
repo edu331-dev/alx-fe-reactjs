bb682dd13c866254dc264c8a1a0d5cd39e28fa66
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 17:26:39 +0300

    Task 2 update 4

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,9 +1,5 @@
-// src/UserContext.js
 import { createContext } from 'react';
 
-// Create the context
 const UserContext = createContext(null);
 
-// Export it so other components can consume it
 export default UserContext;
-
