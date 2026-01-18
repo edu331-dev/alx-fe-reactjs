ffffd17fde1e34ca42ada3c9b95012549a485fe9
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 16:26:42 +0300

    Task 2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,12 +1,13 @@
-// src/App.jsx
-import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      <h1>Simple Counter App</h1>
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
