1916f23eac9008635691038885dad5c66b477470
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 16:33:53 +0300

    update

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,12 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+// src/App.jsx
+import Counter from './components/Counter';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
   );
 }
 
--- alx-react-app-new/src/UserContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-// Create the context
-const UserContext = createContext(null);
-
-export default UserContext;
