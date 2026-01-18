bdd1b13b45b5c040c1122bb85ee3a48703084835
Edwin Gitari <e.kabgogitari@gmail.com>
2026-01-18 14:14:27 +0300

    Task 0 update

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#f5f5f5"
+      }}
+    >
+      <p>Main content goes here.</p>
+    </main>
+  );
+}
+
+export default MainContent;
