a54c2aafa768a523efa3dcb40f18b751de6e3333
Edwin Gitari <e.kabgogitari@gmail.com>
2026-02-20 15:32:42 +0300

    update 0

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ export default function HomePage() {
           Recipe Sharing Platform
         </h1>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {recipes.map((recipe) => (
             <div
               key={recipe.id}
